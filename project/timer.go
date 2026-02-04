8a609a7e5d4ec6709ac17cda7c6be2d694adb589
Isak Kvammen <isakbkv@stud.ntnu.no>
2026-02-04 13:44:09 +0100

    ny

--- project/timer.go
+++ project/timer.go
@@ -7,12 +7,10 @@ var (
 	timerActive  bool
 )
 
-// tilsvarer get_wall_time
 func getWallTime() time.Time {
 	return time.Now()
 }
 
-// starter timeren med varighet i sekunder
 func TimerStart(duration float64) {
 	timerEndTime = getWallTime().Add(
 		time.Duration(duration * float64(time.Second)),
@@ -20,12 +18,10 @@ func TimerStart(duration float64) {
 	timerActive = true
 }
 
-// stopper timeren
 func TimerStop() {
 	timerActive = false
 }
 
-// returnerer true hvis timeren er aktiv og tiden er ute
 func TimerTimedOut() bool {
 	return timerActive && getWallTime().After(timerEndTime)
 }
