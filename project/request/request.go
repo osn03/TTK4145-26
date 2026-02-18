ebf8e42e9fe783c350377bcba1bcc0965f4a1bc1
23 <student@sanntidssalen.no>
2026-02-18 11:51:15 +0100

    started ESM

--- project/request/request.go
+++ project/request/request.go
@@ -14,7 +14,7 @@ type DirnBehaviourPair struct {
 func Above(e elevator.Elevator) bool {
 	for f := e.Floor + 1; f < constant.NumFloors; f++ {
 		for b := elevio.ButtonType(0); b < constant.NumButtons; b++ {
-			if e.Requests[f][b] {
+			if e.Requests[f][b] ==1 {
 				return true
 			}
 		}
@@ -25,7 +25,7 @@ func Above(e elevator.Elevator) bool {
 func Below(e elevator.Elevator) bool {
 	for f := 0; f < e.Floor; f++ {
 		for b := elevio.ButtonType(0); b < constant.NumButtons; b++ {
-			if e.Requests[f][b] {
+			if e.Requests[f][b] == 1 {
 				return true
 			}
 		}
@@ -35,7 +35,7 @@ func Below(e elevator.Elevator) bool {
 
 func Here(e elevator.Elevator) bool {
 	for b := elevio.ButtonType(0); b < 3; b++ {
-		if e.Requests[e.Floor][b] {
+		if e.Requests[e.Floor][b] == 1 {
 			return true
 		}
 	}
@@ -87,13 +87,13 @@ func ShouldStop(e elevator.Elevator) bool {
 	switch e.Dirn {
 
 	case elevio.MD_Up:
-		return e.Requests[e.Floor][elevio.BT_HallUp] ||
-			e.Requests[e.Floor][elevio.BT_Cab] ||
+		return e.Requests[e.Floor][elevio.BT_HallUp] == 1 ||
+			e.Requests[e.Floor][elevio.BT_Cab] == 1 ||
 			!Above(e)
 
 	case elevio.MD_Down:
-		return e.Requests[e.Floor][elevio.BT_HallDown] ||
-			e.Requests[e.Floor][elevio.BT_Cab] ||
+		return e.Requests[e.Floor][elevio.BT_HallDown] == 1 ||
+			e.Requests[e.Floor][elevio.BT_Cab] == 1 ||
 			!Below(e)
 
 	case elevio.MD_Stop:
@@ -114,27 +114,27 @@ func ShouldClearImmediately(e elevator.Elevator, btnFloor int, btnType elevio.Bu
 
 func ClearAtCurrentFloor(e elevator.Elevator) elevator.Elevator {
 
-	e.Requests[e.Floor][elevio.BT_Cab] = false
+	e.Requests[e.Floor][elevio.BT_Cab] = 0
 
 	switch e.Dirn {
 
 	case elevio.MD_Up:
-		if !Above(e) && !e.Requests[e.Floor][elevio.BT_HallUp] {
-			e.Requests[e.Floor][elevio.BT_HallDown] = false
+		if !Above(e) && !(e.Requests[e.Floor][elevio.BT_HallUp] == 1) {
+			e.Requests[e.Floor][elevio.BT_HallDown] = 0
 		}
-		e.Requests[e.Floor][elevio.BT_HallUp] = false
+		e.Requests[e.Floor][elevio.BT_HallUp] = 0	
 
 	case elevio.MD_Down:
-		if !Below(e) && !e.Requests[e.Floor][elevio.BT_HallDown] {
-			e.Requests[e.Floor][elevio.BT_HallUp] = false
+		if !Below(e) && !(e.Requests[e.Floor][elevio.BT_HallDown] == 1) {
+			e.Requests[e.Floor][elevio.BT_HallUp] = 0
 		}
-		e.Requests[e.Floor][elevio.BT_HallDown] = false
+		e.Requests[e.Floor][elevio.BT_HallDown] = 0
 
 	case elevio.MD_Stop:
 		fallthrough
 	default:
-		e.Requests[e.Floor][elevio.BT_HallUp] = false
-		e.Requests[e.Floor][elevio.BT_HallDown] = false
+		e.Requests[e.Floor][elevio.BT_HallUp] = 0
+		e.Requests[e.Floor][elevio.BT_HallDown] = 0
 	}
 
 	return e
